e193ff891a7600800c584ba4b35022a461c52576
David Witkowski <43667775+chodera@users.noreply.github.com>
2023-06-14 17:51:38 +0200

    Replace test value with ref to config

--- tap_dadjokes/client.py
+++ tap_dadjokes/client.py
@@ -41,7 +41,7 @@ class DadJokesStream(RESTStream):
         """
         headers = {}
         if "user_agent" in self.config:
-            headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1" 
+            headers["User-Agent"] = self.config.get("auth_token")
         headers["Accept"] = "application/json"
         return headers
 
